78595c680ffb3aa4b598fb16ed7955fdd3acb2b6
Ainor Syahrizal <macintorch@gmail.com>
2016-12-08 16:14:00 +0800

    animate translation

--- app/src/main/java/ainor/com/my/animations/MainActivity.java
+++ app/src/main/java/ainor/com/my/animations/MainActivity.java
@@ -9,22 +9,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void fade (View view) {
         ImageView nexus = (ImageView) findViewById(R.id.nexus);
-        ImageView nexus1hand = (ImageView) findViewById(R.id.nexushand);
 
-        nexus.animate().alpha(0f).setDuration(2000);
-        nexus1hand.animate().alpha(1f).setDuration(2000);
-        nexus1hand.bringToFront();
+
+        nexus.animate().translationXBy(-1000f).setDuration(2000);
+
+
+//        ImageView nexus1hand = (ImageView) findViewById(R.id.nexushand);
+//        nexus1hand.animate().alpha(1f).setDuration(2000);
     }
 
-    public void fadeagain (View view) {
-        ImageView nexus = (ImageView) findViewById(R.id.nexus);
-        ImageView nexushand1 = (ImageView) findViewById(R.id.nexushand);
 
-        nexushand1.animate().alpha(1f).setDuration(2000);
-        nexus.animate().alpha(0f).setDuration(2000);
-        nexushand1.bringToFront();
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
